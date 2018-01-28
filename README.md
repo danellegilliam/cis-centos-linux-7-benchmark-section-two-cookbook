d47e9fba5b8359e0b6f0bb095b17756a60c3a6a9
Danelle Gilliam <danellegilliam@gmail.com>
2018-01-28 18:21:58 -0500

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# cis-centos-linux-7-benchmark-section-two
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: cis-centos-linux-7-benchmark-section-two
+# Recipe:: default
+#
+# Copyright:: 2018, Danelle Gilliam, All Rights Reserved.
