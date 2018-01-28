48a57f3712866df1a7be5c2899d60496dd84bb61
Danelle Gilliam <danellegilliam@gmail.com>
2018-01-28 18:21:59 -0500

    Add generated delivery build cookbook

---
+++ .delivery/build_cookbook/metadata.rb
@@ -0,0 +1,8 @@
+name 'build_cookbook'
+maintainer 'Danelle Gilliam'
+maintainer_email 'danellegilliam@gmail.com'
+license 'all_rights'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+
+depends 'delivery-truck'
---
+++ .delivery/build_cookbook/recipes/deploy.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook:: build_cookbook
+# Recipe:: deploy
+#
+# Copyright:: 2018, Danelle Gilliam, All Rights Reserved.
+include_recipe 'delivery-truck::deploy'
